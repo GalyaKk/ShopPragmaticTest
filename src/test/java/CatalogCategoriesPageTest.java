65fdecd5a7d4316bed4672e95ef5a34424dab03b
Galya Kyoseva <galyakyoseva88@gmail.com>
2024-06-27 16:13:17 +0300

    made add and delete two separate tests

--- src/test/java/CatalogCategoriesPageTest.java
+++ src/test/java/CatalogCategoriesPageTest.java
@@ -15,18 +15,20 @@ public class CatalogCategoriesPageTest extends BaseTest{
     LoginPageTest loginPageTest = new LoginPageTest();
 
     @Test
-    public void addAndDeleteCategoryTest() {
+    public void addCategoryTest() {
         loginPageTest.loginAsAdmin();
-        WaitTool.waitUntilElementIsClickable(LeftNavigationBar.CATALOG_MENU);
-        LeftNavigationBar.clickOnWebElement(LeftNavigationBar.CATALOG_MENU);
-        WaitTool.waitUntilElementIsClickable(LeftNavigationBar.CATALOG_MENU_CATEGORIES);
-        LeftNavigationBar.clickOnWebElement(LeftNavigationBar.CATALOG_MENU_CATEGORIES);
+        CatalogCategoriesPage.afterLoginGoToCatalogCategoriesPage();
         CatalogCategoriesPage.clickOnWebElement(CatalogCategoriesPage.ADD_CATEGORY_BUTTON);
         CatalogCategoriesPage.writeTextInWebelemnt(CatalogCategoriesPage.CATEGORY_NAME_INPUT_FIELD, CatalogCategoriesPage.newCategoryName);
         Action.scrollToMyElement(CatalogCategoriesPage.META_TAG_TITLE_INPUT_FIELD);
         CatalogCategoriesPage.writeTextInWebelemnt(CatalogCategoriesPage.META_TAG_TITLE_INPUT_FIELD, "newly added category");
         CatalogCategoriesPage.clickOnWebElement(CatalogCategoriesPage.SAVE_ADDED_CATEGORY_BUTTON);
         Assert.assertEquals(CatalogCategoriesPage.getTextFromSuccessMessage(), "Success: You have modified categories!");
+    }
+    @Test
+    public void deleteCategoryTest(){
+        loginPageTest.loginAsAdmin();
+        CatalogCategoriesPage.afterLoginGoToCatalogCategoriesPage();
         CatalogCategoriesPage.clickOnWebElement(CatalogCategoriesPage.CATEGORY_TYPE);
         CatalogCategoriesPage.clickOnWebElement(CatalogCategoriesPage.DELETE_BUTTON);
         try{
